c77d07168eca246e563a47da2e2aada42dcf449b
Christian Sumido <gcbsumid@gmail.com>
2024-04-20 09:40:31 -0700

    update for local machines

--- start-xdaemon.sh
+++ start-xdaemon.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./home/ubuntu/xelis-blockchain/target/release/xelis_daemon
+./home/christian/xelis-blockchain/target/release/xelis_daemon
--- start-xminer.sh
+++ start-xminer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./home/ubuntu/xelis-blockchain/target/release/xelis_miner --miner-address xel:7gkkkzkduncny949plz8dhna90hhmcug6vtrwppd0zntkclg84psql0wxqg
+./home/christian/xelis-blockchain/target/release/xelis_miner --miner-address xel:7gkkkzkduncny949plz8dhna90hhmcug6vtrwppd0zntkclg84psql0wxqg
